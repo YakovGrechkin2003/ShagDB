888eee6e9317c77b97ee50977eba1cc0468815b3
SergeyOlisov <58857959+SergeyOlisov@users.noreply.github.com>
2020-03-09 11:29:26 +0300

    Update moc_faculty.cpp

--- build-itstep_db-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/debug/moc_faculty.cpp
+++ build-itstep_db-Desktop_Qt_5_12_5_MinGW_64_bit-Debug/debug/moc_faculty.cpp
@@ -38,7 +38,6 @@ QT_MOC_LITERAL(0, 0, 7) // "faculty"
 #undef QT_MOC_LITERAL
 
 static const uint qt_meta_data_faculty[] = {
-
  // content:
        8,       // revision
        0,       // classname
